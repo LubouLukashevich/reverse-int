2ba6852a48f0eb5d4abcded108aa4d5e659e07f1
Lubou Lukashevich <lukashevich.rs@gmail.com>
2022-07-03 23:11:42 +0300

    init: solve task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+    let nString = '';
+    (n < 0) ? nString =  -n + '' : nString = n + '';
+    let result = '';
+    for (var i = nString.length - 1; i >= 0; i -= 1) {
+     result = result + nString[i];
+    }
+  return result;
 }
